7717f9ae6b667a20958898d86a70863456cf9692
Bart Massey <bart@cs.pdx.edu>
2018-08-03 00:44:53 -0700

    modified to use thread per request

--- src/main.rs
+++ src/main.rs
@@ -5,25 +5,32 @@
 
 extern crate byteorder;
 extern crate chrono;
+#[macro_use] extern crate lazy_static;
 
 use byteorder::{BigEndian, WriteBytesExt};
 use chrono::naive;
 
-use std::io;
 use std::net::TcpListener;
+use std::thread;
 
-/// Process time requests.
-fn main() -> io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:37").unwrap();
-    let epoch = naive::NaiveDate::from_ymd(1900, 1, 1)
+lazy_static! {
+    static ref EPOCH: i64 = naive::NaiveDate::from_ymd(1900, 1, 1)
         .and_hms(0, 0, 0)
         .timestamp();
+}
+
+/// Process time requests.
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:37").unwrap();
 
     // accept connections and process them serially
     for stream in listener.incoming() {
-        let mut stream = stream?;
-        let mut now = chrono::Utc::now().timestamp() - epoch;
-        stream.write_u32::<BigEndian>(now as u32)?;
+        thread::spawn(|| {
+            let mut stream = stream
+                .expect("could not start stream");
+            let now = chrono::Utc::now().timestamp() - *EPOCH;
+            stream.write_u32::<BigEndian>(now as u32)
+                .expect("could not write to stream");
+        });
     }
-    Ok(())    
 }
