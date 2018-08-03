303ba6eb46738a9cde5bf7843bedcf8cdb43159d
Bart Massey <bart@cs.pdx.edu>
2018-08-03 13:22:48 -0700

    got rid of global, lazy_static

--- src/main.rs
+++ src/main.rs
@@ -9,42 +9,35 @@
 
 extern crate byteorder;
 extern crate chrono;
-#[macro_use] extern crate lazy_static;
 
 use byteorder::{BigEndian, WriteBytesExt};
 use chrono::naive;
 
 use std::{net, thread};
 
-lazy_static! {
-    static ref EPOCH: i64 = naive::NaiveDate::from_ymd(1900, 1, 1)
-        .and_hms(0, 0, 0)
-        .timestamp();
-}
-
 /// Process TCP time requests.
-fn tcp_handler() {
+fn tcp_handler(epoch: i64) {
     let listener = net::TcpListener::bind("127.0.0.1:37").unwrap();
 
     // accept connections and process them serially
     for stream in listener.incoming() {
         let mut stream = stream
             .expect("could not start stream");
-        let now = chrono::Utc::now().timestamp() - *EPOCH;
+        let now = chrono::Utc::now().timestamp() - epoch;
         stream.write_u32::<BigEndian>(now as u32)
             .expect("could not write to stream");
     }
 }
 
 /// Process UDP time requests.
-fn udp_handler() {
+fn udp_handler(epoch: i64) {
     let socket = net::UdpSocket::bind("127.0.0.1:37").unwrap();
     loop {
         let mut buf = [0; 0];
         let (amt, src) = socket.recv_from(&mut buf)
             .expect("bad request packet");
         assert_eq!(amt, 0);
-        let now = chrono::Utc::now().timestamp() - *EPOCH;
+        let now = chrono::Utc::now().timestamp() - epoch;
         let mut buf: Vec<u8> = Vec::with_capacity(4);
         buf.write_u32::<BigEndian>(now as u32)
             .expect("could not create packet");
@@ -54,8 +47,11 @@ fn udp_handler() {
 }
 
 fn main() {
-    let tcp_id = thread::spawn(|| tcp_handler());
-    let udp_id = thread::spawn(|| udp_handler());
+    let epoch = naive::NaiveDate::from_ymd(1900, 1, 1)
+        .and_hms(0, 0, 0)
+        .timestamp();
+    let tcp_id = thread::spawn(move || tcp_handler(epoch));
+    let udp_id = thread::spawn(move || udp_handler(epoch));
     tcp_id.join().expect("TCP thread failed");
     udp_id.join().expect("UDP thread failed");
 }
